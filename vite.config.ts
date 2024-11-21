463179e35fb923fbc00afcba2f4530885f27975f
Josemario <97921175+Josemario17@users.noreply.github.com>
2024-11-21 05:57:56 +0100

    bug fix: routes vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist'
+  },
+  base: './',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+      '@': '/src'
+    }
+  }
+});
